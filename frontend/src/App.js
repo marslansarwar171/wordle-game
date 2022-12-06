4a7800c63be7079db7d2294723c4c7c92a0595cd
marslansarwar171 <masarwar171@gmail.com>
2022-12-06 16:09:10 +0500

    changes vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,9 @@ function App() {
   const [solution, setSolution] = useState(null)
   
   useEffect(() => {
-    fetch('http://localhost:3001/solutions')
+    fetch('http://localhost:8000/wordle-list/')
       .then(res => res.json())
       .then(json => {
-        // random int between 0 & 14
         const randomSolution = json[Math.floor(Math.random()*json.length)]
         setSolution(randomSolution.word)
     })
