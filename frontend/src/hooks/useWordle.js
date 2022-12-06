a1c7a87603efee0170775fcbad03c46b6a51c92c
marslansarwar171 <masarwar171@gmail.com>
2022-12-06 19:41:13 +0500

    temporary word removed from guesses

--- frontend/src/hooks/useWordle.js
+++ frontend/src/hooks/useWordle.js
@@ -4,7 +4,7 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0) 
   const [currentGuess, setCurrentGuess] = useState('')
   const [guesses, setGuesses] = useState([...Array(6)]) // each guess is an array
-  const [history, setHistory] = useState(['ninja']) // each guess is a string
+  const [history, setHistory] = useState([]) // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false)
   const [usedKeys, setUsedKeys] = useState({}) 
 
