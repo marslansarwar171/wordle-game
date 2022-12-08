13a86f912caa97ef83beb05b98ffc95e8b40b00a
marslansarwar171 <masarwar171@gmail.com>
2022-12-08 19:58:19 +0500

    local word list added

--- frontend/src/constants/words.js
+++ frontend/src/constants/words.js
@@ -1,6 +1,13 @@
-const WORD_LIST = 
-["ninja", "plate", "plain", "pours", "pools", "spade", "drive", "relax", "times", "train", 
- "cores", "blame", "banks", "phone", "bling", "hello", "coins", "apple"
-];
+const WORD_LIST =
+    ["ninja", "plate", "plain", "pours", "pools", "spade", "drive", "relax", "times", "train",
+        "cores", "blame", "banks", "phone", "bling", "hello", "coins", "apple", "abuse", "agent",
+        "adult", "award", "begin", "radio", "range", "price", "power", "reply", "beach", "basis",
+        "anger", "allow", "agree", "adopt", "admit", "above", "about", "since", "while", "blood",
+        "worth", "other", "right", "river", "rugby", "scale", "route", "start", "power", "trend",
+        "trial", "voice", "watch", "avoid", "break", "build", "bring", "visit", "worry", "write",
+        "teach", "study", "urban", "upset", "upper", "price", "super", "third", "prime", "valid",
+        "vague", "wrong", "young", "local", "minor", "moral", "extra", "false", "fifth", "final",
+        "first", "fresh", "happy", "eight", "theme", "earth", "until", "dress", "dream", "drama"
+    ];
 
 export default WORD_LIST;
