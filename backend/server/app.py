6b823ad784ac67c8e2e1bdbaa4b1d85901c0b2ce
marslansarwar171 <masarwar171@gmail.com>
2022-12-08 16:16:18 +0500

    reload state True

--- backend/server/app.py
+++ backend/server/app.py
@@ -58,4 +58,4 @@ async def list_wordles():
 
 
 if __name__ == "__main__":
-  uvicorn.run("app:app", host="0.0.0.0", port=8000, reload='true')
+  uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
